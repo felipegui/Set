43d954c7a664648f065f9212d57f0ee02a669d33
Felipe Guimaraes <fguiengenharia@gmail.com>
2025-04-19 14:25:28 -0300

    Usando TreeSet: Classe Product tem que ser uma implementacao do do ComparableTo, para possa ocorrer ordenacao

--- Set/src/application/Program.java
+++ Set/src/application/Program.java
@@ -1,20 +1,21 @@
 package application;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 import entities.Product;
 
 public class Program {
     public static void main(String[] args) throws Exception {
 
-        Set<Product> set = new HashSet<>();
+        Set<Product> set = new TreeSet<>();
 
         set.add(new Product("TV", 900.0));
         set.add(new Product("Notebook", 1200.0));
         set.add(new Product("Tablet", 900.0));
 
-        Product prod = new Product("Notebook", 1200.0);
-        System.out.println(set.contains(prod));
+        for (Product p : set) {
+            System.out.println(p);
+        }
     }
 }
